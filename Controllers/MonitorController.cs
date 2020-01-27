fd66b79774de8036ff9d9c4749722dcb034dc401
Louis White <louis.white1500@gmail.com>
2020-01-27 12:30:10 +0800

    Initial commit

---
+++ Controllers/MonitorController.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.NetworkInformation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace R6Stats.NET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MonitorController : ControllerBase
+    {
+        [HttpGet]
+        public long Ping()
+        {
+            Ping pinger = null;
+            long ping = 0;
+
+            try
+            {
+                pinger = new Ping();
+                var reply = pinger.Send("8.8.8.8");
+                ping = reply.RoundtripTime;
+
+            }
+            catch(PingException)
+            {
+                // Discard ping exceptions
+            }
+            finally
+            {
+                if (pinger != null)
+                    pinger.Dispose();
+            }
+
+            return ping;
+        }
+    }
+}
---
+++ Controllers/R6StatsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using R6Tab.NET.Models;
+
+namespace R6Stats.NET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class R6StatsController : ControllerBase
+    {
+        [HttpGet("playerByNameAndPlatform")]
+        public NameResults GetPlayerByNameAndPlatform()
+        {
+            // TODO: Flesh out endpoint
+            return new NameResults();
+        }
+    }
+}
