0c00570bb44fde8ebdcc2ce23fb8759aa6214cf6
Louis White <louis.white1500@gmail.com>
2020-03-16 21:25:03 +0800

    Added Player Search component and page to vue app

---
+++ R6Stats.NET/client-app/src/views/PlayerSearch.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="playerSearch">
+    <PlayerSearch msg="Player Search" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import PlayerSearch from "@/components/PlayerSearch.vue";
+
+export default {
+  name: "playerSearch",
+  components: {
+    PlayerSearch
+  }
+};
+</script>
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # R6Stats.NET
+
+# Testing R6Stats API Endpoint:
+
+curl -v https://localhost:5001/r6stats/player/<platform>/<playerName>
+curl -v https://localhost:5001/r6stats/player/<playerId>
