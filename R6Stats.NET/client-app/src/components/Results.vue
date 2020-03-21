b19e8c6c6f539793bf8740214a0c95ab22730d38
Louis White <louis.white1500@gmail.com>
2020-03-21 15:29:36 +0800

    Added no results found message

--- R6Stats.NET/client-app/src/components/Results.vue
+++ R6Stats.NET/client-app/src/components/Results.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="card-columns" >
+    <div v-if="results.totalResults === 0">
+      No results found.
+    </div>
     <div class="card" v-for="item in results.playerResults" :key="item.playerId">
       <div class="card-body">
         <h5 class="card-title">{{item.playerName}}</h5>
--- R6Stats.NET/client-app/src/views/Home.vue
+++ R6Stats.NET/client-app/src/views/Home.vue
@@ -4,12 +4,13 @@
     <div class="container-fluid">
       <div class="row mx-auto">
         <!-- Search section -->
-        <section class="col-sm-12 pt-3 px-0">
-          <search @search=onSearch></search>
+        <section class="col-sm-12">
+          <search @search="onSearch"></search>
         </section>
-        <!-- Loading -->
+        <br/>
+        <!-- Loading section -->
         <div v-if="loading" class="spinner-border" role="status">
-            <span v-if="loading" class="sr-only">Loading...</span>
+            <span class="sr-only">Loading...</span>
         </div>
         <!-- Results section -->
         <section class="results">
@@ -33,16 +34,18 @@ import Results from "../components/Results.vue";
 })
 export default class App extends Vue {
   @Provide() results = [];
-  @Prop() loading!:boolean;
+  @Prop() loading = false;
   
   async onSearch(term:string) {
     this.loading = true;
     this.results = [];
-    await Vue.axios.get(`https://localhost:5001/r6stats/player/uplay/${term}`).then((response) =>
-    {
-      this.loading=false;
-      this.results = response.data;
-    })
+    await Vue.axios
+      .get(`https://localhost:5001/r6stats/player/uplay/${term}`)
+      .then((response) =>
+      {
+        this.loading=false;
+        this.results = response.data;
+      });
   }
 }
 </script>
