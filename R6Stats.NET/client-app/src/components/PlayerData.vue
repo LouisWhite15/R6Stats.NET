0c8b83ef70f34655cfbd010b4d1c2e9ff2a12d35
Louis White <louis.white1500@gmail.com>
2020-04-07 17:10:10 +0800

    more styling of the player stats page

--- R6Stats.NET/client-app/src/components/PlayerData.vue
+++ R6Stats.NET/client-app/src/components/PlayerData.vue
@@ -5,48 +5,60 @@
     </div>
     <br>
     <b-container> 
-      <img :src="'https://ubisoft-avatars.akamaized.net/' + result.player.id + '/default_256_256.png'" width="100" height="100">
-      <h3>{{result.player.name}}</h3>
-      <b-row>
-        <b-col>CURRENT MMR</b-col>
-        <b-col>CURRENT RANK</b-col>
-        <b-col>MAXIMUM MMR</b-col>
-        <b-col>MAXIMUM RANK</b-col>
-      </b-row>
-      <b-row>
-        <b-col>{{result.ranked.asiaMmr}}</b-col>
-        <b-col>{{result.ranked.asiaRankName}}</b-col>
-        <b-col>{{result.ranked.asiaMaxMmr}}</b-col>
-        <b-col>{{result.ranked.asiaMaxRankName}}</b-col>
+      <img style="padding:10px" :src="'https://ubisoft-avatars.akamaized.net/' + result.player.id + '/default_256_256.png'" width="150" height="150">
+      <b-row class="bg-light" style="padding:10px">
+        <b-col style="text-align:right">Name:<br>Level:<br>Last updated:</b-col>
+        <b-col style="text-align:left">{{result.player.name}}<br>Coming soon!<br>Coming soon!</b-col>
       </b-row>
       <br>
       <b-row>
-        <b-col>KILLS</b-col>
-        <b-col>DEATHS</b-col>
-        <b-col>K/D</b-col>
-      </b-row>
-      <b-row>
-        <b-col>{{result.ranked.asiaKills}}</b-col>
-        <b-col>{{result.ranked.asiaDeaths}}</b-col>
-        <b-col>{{result.ranked.asiaKd}}</b-col>
-      </b-row>
-      <br>
-      <b-row>
-        <b-col>WINS</b-col>
-        <b-col>LOSSES</b-col>
-        <b-col>W/L</b-col>
-      </b-row>
-      <b-row>
-        <b-col>{{result.ranked.asiaWins}}</b-col>
-        <b-col>{{result.ranked.asiaLosses}}</b-col>
-        <b-col>{{result.ranked.asiaWinLossPercentage}}</b-col>
-      </b-row>
-      <br>
-      <b-row>
-        <b-col>MMR CHANGE</b-col>
-      </b-row>
-      <b-row>
-        <b-col><p v-if="result.ranked.asiaMmrChange > 0">+{{result.ranked.asiaMmrChange}}</p><p v-if="result.ranked.asiaMmrChange < 0">+{{result.ranked.asiaMmrChange}}</p></b-col>
+        <b-col>
+          <!-- PLACEHOLDER IMAGE -->
+          <img src="https://trackercdn.com/cdn/r6.tracker.network/ranks/svg/hd-rank17.svg" width="300" height="300">
+          </b-col>
+        <b-col>
+          <b-row class="bg-light">
+            <b-col>CURRENT MMR</b-col>
+            <b-col>CURRENT RANK</b-col>
+            <b-col>MAXIMUM MMR</b-col>
+            <b-col>MAXIMUM RANK</b-col>
+          </b-row>
+          <b-row class="bg-light">
+            <b-col>{{result.ranked.asiaMmr}}</b-col>
+            <b-col>{{result.ranked.asiaRankName}}</b-col>
+            <b-col>{{result.ranked.asiaMaxMmr}}</b-col>
+            <b-col>{{result.ranked.asiaMaxRankName}}</b-col>
+          </b-row>
+          <br>
+          <b-row class="bg-light">
+            <b-col>KILLS</b-col>
+            <b-col>DEATHS</b-col>
+            <b-col>K/D</b-col>
+          </b-row>
+          <b-row class="bg-light">
+            <b-col>{{result.ranked.asiaKills}}</b-col>
+            <b-col>{{result.ranked.asiaDeaths}}</b-col>
+            <b-col>{{result.ranked.asiaKd}}</b-col>
+          </b-row>
+          <br>
+          <b-row class="bg-light">
+            <b-col>WINS</b-col>
+            <b-col>LOSSES</b-col>
+            <b-col>W/L</b-col>
+          </b-row>
+          <b-row class="bg-light" >
+            <b-col>{{result.ranked.asiaWins}}</b-col>
+            <b-col>{{result.ranked.asiaLosses}}</b-col>
+            <b-col>{{result.ranked.asiaWinLossPercentage}}</b-col>
+          </b-row>
+          <br>
+          <b-row class="bg-light">
+            <b-col>MMR CHANGE</b-col>
+          </b-row>
+          <b-row class="bg-light">
+            <b-col><p v-if="result.ranked.asiaMmrChange > 0">+{{result.ranked.asiaMmrChange}}</p><p v-if="result.ranked.asiaMmrChange < 0">+{{result.ranked.asiaMmrChange}}</p></b-col>
+          </b-row>
+        </b-col>
       </b-row>
     </b-container>
   </div>
