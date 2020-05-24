fb581228cfc62ee6f6a89ced1e61d2ded0223eac
Louis White <louis.white1500@gmail.com>
2020-05-24 15:59:37 +0800

    feat: added match history

--- R6Stats.NET/client-app/src/components/PlayerData.vue
+++ R6Stats.NET/client-app/src/components/PlayerData.vue
@@ -71,6 +71,45 @@
               <b-col><p style="color: green" v-if="result.ranked.asiaMmrChange > 0">+{{result.ranked.asiaMmrChange}}</p><p style="color: red" v-if="result.ranked.asiaMmrChange < 0">{{result.ranked.asiaMmrChange}}</p></b-col>
             </b-row>
           </b-card>
+          <br>
+          <b-card
+            title="Match History"
+            style="min-width: 100vh">
+            <b-container v-for="item in matchHistory" :key="item" class="mx-auto">
+              <b-row class="bg-light title">
+                <b-col style="font-size: 20px">{{item.date}}</b-col>
+              </b-row>
+              <b-row class="bg-light title">
+                <b-col>MATCHES</b-col>
+                <b-col>WINS</b-col>
+                <b-col>LOSSES</b-col>
+              </b-row>
+              <b-row class="bg-light data">
+                <b-col>{{item.rankedMatches}}</b-col>
+                <b-col>{{item.rankedWins}}</b-col>
+                <b-col>{{item.rankedLosses}}</b-col>
+              </b-row>
+              <b-row class="bg-light title">
+                <b-col>KILLS</b-col>
+                <b-col>DEATHS</b-col>
+                <b-col>K/D</b-col>
+                <b-col>HEADSHOT %</b-col>
+              </b-row>
+              <b-row class="bg-light data">
+                <b-col>{{item.rankedKills}}</b-col>
+                <b-col>{{item.rankedDeaths}}</b-col>
+                <b-col>{{item.rankedKd}}</b-col>
+                <b-col>{{item.headshotAccuracy}}</b-col>
+              </b-row>
+              <b-row class="bg-light title">
+                <b-col>MMR CHANGE</b-col>
+              </b-row>
+              <b-row class="bg-light data">
+                <b-col><p style="color: green" v-if="item.asiaMmrChange > 0">+{{item.asiaMmrChange}}</p><p style="color: red" v-if="item.asiaMmrChange < 0">{{item.asiaMmrChange}}</p><p v-if="item.asiaMmrChange == 0">{{item.asiaMmrChange}}</p></b-col>
+              </b-row>
+              <br>
+            </b-container>
+          </b-card>
         </b-col>
       </b-row>
     </b-container>
@@ -90,6 +129,10 @@ export default class PlayerData extends Vue {
 
   get seasonsData() {
     return Object.values(this.data.seasons).reverse();
+  }
+
+  get matchHistory() {
+    return Object.values(this.data.matchHistory).slice(-5);
   }
 }
 </script>
