0f992e473afa39d43d2dc0f8f313c436a531e2c5
Louis White <louis.white1500@gmail.com>
2020-05-31 17:29:46 +0800

    refactor: only retrieve r6tab api key from config once

--- R6Stats.NET/Controllers/R6StatsController.cs
+++ R6Stats.NET/Controllers/R6StatsController.cs
@@ -15,14 +15,14 @@ namespace R6Stats.NET.Controllers
     public class R6StatsController : ControllerBase
     {
         private readonly IR6TabApi _r6TabApi;
-        private readonly IConfiguration _configuration;
+        private readonly string _r6TabApiKey;
 
         public R6StatsController(
             IR6TabApi r6TabApi,
             IConfiguration configuration)
         {
             _r6TabApi = r6TabApi;
-            _configuration = configuration;
+            _r6TabApiKey = configuration.GetValue<string>("R6Tab_ApiKey");
         }
 
         [HttpGet("search/{platform}/{name}")]
@@ -35,21 +35,21 @@ namespace R6Stats.NET.Controllers
                 return BadRequest();
             }
 
-            var result = await _r6TabApi.SearchByName(name, parsedPlatform, _configuration["R6Tab_ApiKey"]);
+            var result = await _r6TabApi.SearchByName(name, parsedPlatform, _r6TabApiKey);
             return Ok(result);
         }
 
         [HttpGet("player/{id}")]
         public async Task<IActionResult> GetPlayerByIdAsync(Guid id)
         {
-            var result = await _r6TabApi.SearchById(id, _configuration["R6Tab_ApiKey"]);
+            var result = await _r6TabApi.SearchById(id, _r6TabApiKey);
             return Ok(result);
         }
 
         [HttpGet("update/{id}")]
         public async Task<IActionResult> UpdatePlayerByIdAsync(Guid id)
         {
-            await _r6TabApi.UpdatePlayerDataById(id, _configuration["R6Tab_ApiKey"]);
+            await _r6TabApi.UpdatePlayerDataById(id, _r6TabApiKey);
             return Ok();
         }
     }
